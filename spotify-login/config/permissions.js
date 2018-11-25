d93f7082b0528fbe1cb3fb27ed04819315800258
Ric-Lavers <ric_lavers@outlook.com>
2018-11-25 12:54:35 +1100

    updated server

---
+++ spotify-login/config/permissions.js
@@ -0,0 +1,42 @@
+const allPermisions = [
+  "streaming",
+  "streaming",
+  "user-follow-modify",
+  "user-follow-modify",
+  "playlist-read-private",
+  "playlist-read-private",
+  "user-modify-playback-state",
+  "user-modify-playback-state",
+  "user-read-private",
+  "user-read-private",
+  "user-read-birthdate",
+  "user-read-birthdate",
+  "user-library-read",
+  "user-library-read",
+  "playlist-modify-private",
+  "playlist-modify-private",
+  "app-remote-control",
+  "app-remote-control",
+  "user-follow-read",
+  "user-follow-read",
+  "user-top-read",
+  "user-top-read",
+  "playlist-read-collaborative",
+  "playlist-read-collaborative",
+  "user-read-email",
+  "user-read-email",
+  "playlist-modify-public",
+  "playlist-modify-public",
+  "user-read-playback-state",
+  "user-read-playback-state",
+  "user-read-recently-played",
+  "user-read-recently-played",
+  "user-read-currently-playing",
+  "user-read-currently-playing",
+  "user-library-modify",
+  "user-library-modify"
+].join(' ')
+
+module.exports = {
+  allPermisions
+}
