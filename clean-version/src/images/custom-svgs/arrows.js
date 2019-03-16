933b6fd229a24bdb2855680e09f6b9a679666276
Ric Lavers <rlavers@deloitte.com.au>
2019-03-16 15:01:59 +1100

    arrows

---
+++ clean-version/src/images/custom-svgs/arrows.js
@@ -0,0 +1,19 @@
+import up from './arrow-up.svg'
+import down from './arrow-down.svg'
+import left from './arrow-left.svg'
+import right from './arrow-right.svg'
+import { ReactComponent as Up } from './arrow-up.svg'
+import { ReactComponent as Down } from './arrow-down.svg'
+import { ReactComponent as Left } from './arrow-left.svg'
+import { ReactComponent as Right } from './arrow-right.svg'
+
+export {
+	up,
+	down,
+	left,
+	right,
+	Up,
+	Down,
+	Left,
+	Right,
+}
