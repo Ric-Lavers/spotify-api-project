587d779071c9ee33b3cdbea75c100904c8029273
Ric Lavers <rlavers@deloitte.com.au>
2019-03-16 18:41:38 +1100

    more playlists

---
+++ clean-version/src/components/common/SfCheck.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {  ReactComponent as Tick } from '../../images/custom-svgs/tick.svg'
+import {  ReactComponent as Cross } from '../../images/custom-svgs/cross.svg'
+import {  ReactComponent as Plus } from '../../images/custom-svgs/plus.svg'
+
+
+
+
+const SfCheck = ({checked, onClick}) => 
+	<div className="sfCheck" onClick={onClick} >
+		{checked ?
+			<>
+				<Tick className='icon sfCheck_tick ' />
+				<Cross className='icon sfCheck_cross' />
+			</> :
+			<Plus className='icon sfCheck_plus' />
+		}
+		
+		
+	</div>
+
+export default SfCheck;
