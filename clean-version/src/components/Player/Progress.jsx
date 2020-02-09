074dcb761d726693ce7ee8b7b7516a5424cb3b51
Ric-Lavers <ric_lavers@outlook.com>
2020-02-09 21:50:34 +1100

    lint

--- clean-version/src/components/Player/Progress.jsx
+++ clean-version/src/components/Player/Progress.jsx
@@ -1,49 +1,48 @@
-import React, { useState, useEffect, useContext  } from 'react'
-import { CurrentPlayingContext } from '../../context'
-import { seek } from '../../api/spotify'
-
+import React, { useState, useEffect, useContext } from "react";
+import { CurrentPlayingContext } from "../../context";
+import { seek } from "../../api/spotify";
 
 const ProgressContainer = () => {
-  const song = useContext(CurrentPlayingContext)
+  const song = useContext(CurrentPlayingContext);
 
-  if ( !song ) { return null }
-  let { progress_ms } = song
-  let { duration_ms } = song.item
+  if (!song) {
+    return null;
+  }
 
-  return <Progress progress_ms={progress_ms} duration_ms={duration_ms} />
-}
+  let { progress_ms } = song;
+  let { duration_ms } = song.item;
+
+  return <Progress progress_ms={progress_ms} duration_ms={duration_ms} />;
+};
 
 const Progress = React.memo(({ progress_ms, duration_ms }) => {
-  
   const findRange = currentSong => {
-    return (
-      Math.floor( progress_ms/ duration_ms * 100 )
-    )
-  }
+    return Math.floor((progress_ms / duration_ms) * 100);
+  };
 
-  let [ rangeValue, setRange ] = useState(findRange())
+  let [rangeValue, setRange] = useState(findRange());
 
   useEffect(() => {
-    setRange(findRange())
-  }, [progress_ms, duration_ms])
-  
+    setRange(findRange());
+  }, [progress_ms, duration_ms]);
+
   const handleSeek = ({ target: { value } }) => {
     seek({
-      position_ms: Math.floor( value * 0.01 * duration_ms)
-    })
-  }
+      position_ms: Math.floor(value * 0.01 * duration_ms)
+    });
+  };
 
   return (
     <input
-      onChange={({ target: { value } }) => setRange( value )}
+      onChange={({ target: { value } }) => setRange(value)}
       onMouseUp={handleSeek}
       className="icon seek"
       type="range"
-      min='0'
-      max='100'
+      min="0"
+      max="100"
       value={rangeValue.toString()}
     />
-  )
-})
+  );
+});
 
-export default ProgressContainer
+export default ProgressContainer;
