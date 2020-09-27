8c17df4a4abcb30c4074bf2b63bd2b092392e71b
Ric Lavers <rlavers@deloitte.com.au>
2020-09-28 09:35:39 +1000

    getOneAudioFeature hook

---
+++ clean-version/src/components/blacklist/useBlackList.ts
@@ -0,0 +1,23 @@
+const BlackList = {
+  ids: [],
+}
+
+interface limitList {
+  ids: string[]
+  tracks: {
+    [key: string]: {
+      name: string
+      album: string
+      artists: string
+      index: number | string
+    }
+  }
+}
+function useBlackList(): string[] {
+  function append() {}
+  function read() {
+    BlackList
+  }
+
+  return ["BlackList"]
+}
